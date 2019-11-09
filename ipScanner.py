fb32cffafbae18036289296edef1d36261c140a0
sachinreddy1 <sachinreddy1009@gmail.com>
2019-11-08 19:33:04 -0600

    IP sweeper test added.

--- ipScanner.py
+++ ipScanner.py
@@ -1,10 +1,22 @@
 import subprocess 
-  
+import socket
+
 for ping in range(1,10): 
     address = "192.168.1." + str(ping) 
-    res = subprocess.call(['ping', '-c', '3', address]) 
+    res = subprocess.call(['ping', '-q', '-c', '1', address]) 
     if res == 0: 
-        print( "ping to", address, "OK") 
+        print( "ping to", address, "OK")
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+	        s.connect((address, 23))
+	        s.send("Something.")
+	        print address
+	        print "----------> Connection successful. <----------"
+    	except Exception, e:
+        	print 'Connection failed.'
+        s.close() 
+
     elif res == 2: 
         print("no response from", address) 
     else: 
