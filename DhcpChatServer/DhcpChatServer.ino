32f7ce8a7b85e63a5ceab436674de3a73662569d
sachinreddy1 <sachinreddy1009@gmail.com>
2019-11-14 16:46:55 -0600

    Firmware cleaned.

--- DhcpChatServer/DhcpChatServer.ino
+++ DhcpChatServer/DhcpChatServer.ino
@@ -3,7 +3,7 @@
 #include <ArduinoJson.h>
 
 byte mac[] = {
-  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
+  0x00, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE
 };
 IPAddress ip(192, 168, 1, 177);
 IPAddress myDns(192, 168, 1, 1);
@@ -11,31 +11,19 @@ IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 0, 0);
 
 EthernetServer server(23);
-boolean gotAMessage = false; 
 
 const int CAPACITY = 200;
 
 void setup() {
-
   Serial.begin(9600);
-  while (!Serial) {
-    ;
-  }
 
   Serial.println("Trying to get an IP address using DHCP");
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-      while (true) {
-        delay(1);
-      }
-    }
-    if (Ethernet.linkStatus() == LinkOFF) {
-      Serial.println("Ethernet cable is not connected.");
-    }
+  int ethernetVal = Ethernet.begin(mac);
+  
+  if (ethernetVal == 0) {
     Ethernet.begin(mac, ip, myDns, gateway, subnet);
   }
+  
   Serial.print("My IP address: ");
   Serial.println(Ethernet.localIP());
   
@@ -46,12 +34,6 @@ void loop() {
   EthernetClient client = server.available();
 
   if (client) {
-    if (!gotAMessage) {
-      Serial.println("We have a new client");
-      client.println("Hello, client!");
-      gotAMessage = true;
-    }
-
     // Create JSON Object
     StaticJsonDocument<CAPACITY> root;
     root["V1"] = 1.0;
